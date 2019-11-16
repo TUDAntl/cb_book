0a1dbd9c90f67b2cabbdbef721b08b73c1afc387
TUDAntl <25935855+TUDAntl@users.noreply.github.com>
2019-11-16 06:29:44 +0100

    layout:false is needed!

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -9,7 +9,7 @@ router.get('/', (req,res)=>{
         if(!err) {
         }
     });
-    res.render(__dirname.substring(0, __dirname.length-6)+'views/index',{title: "Reservierungsbuch", time1:18, name1:"Thomas", pax1:6, table1:11, time1:"18:00", 
+    res.render(__dirname.substring(0, __dirname.length-6)+'views/index',{layout: false, title: "Reservierungsbuch", time1:18, name1:"Thomas", pax1:6, table1:11, time1:"18:00", 
                                                                                                                 name2:"Brunner", pax2:5, table2:4, time2:"20:15", description2:"geht ins Theater"})});
 
     module.exports = router;
