a5430eb6f3f7013f5609e4cb4f1f57b1a3352f93
TUDAntl <25935855+TUDAntl@users.noreply.github.com>
2019-11-19 03:18:02 +0100

    made the input fail safe

--- routes/dateRoute.js
+++ routes/dateRoute.js
@@ -11,6 +11,7 @@ function sleep(ms) {
 
 
 router.get('/:date', async (req, res) => {
+    terror = false;
     var day = req.params.date.slice(0, 2);
     var month = req.params.date.slice(3, 5);
     var year = req.params.date.slice(6, 10);
@@ -32,31 +33,52 @@ router.get('/:date', async (req, res) => {
 
     console.log(reservierungen);
     db.close();
-    var jdata = { layout: false, title: "Reservierungsbuch", allres: reservierungen };
+    var jdata = { layout: false, title: "Reservierungsbuch", allres: reservierungen, error: terror };
     res.render(__dirname.substring(0, __dirname.length - 6) + 'views/index', jdata);
 
 });
 
 
 router.post('/', (req, res) => {
-
+    terror = false;
     let db = new sqlite3.Database(sqlpath, (err) => {
         if (err) {
             console.log("no succses");
             return console.error(err.message);
         }
     });
-    var sqlcommand = " INSERT INTO reservierungen Values";
-    var values = '(' + String(req.body.date) + ',' + String(req.body.time) + ',"' + String(req.body.name) + '",' + String(req.body.pax) + ',' + String(req.body.tisch) + ',' + '"' + String(req.body.desc) + '" )';
-    console.log(sqlcommand, values);
-    db.run(sqlcommand + values);
+    var parsedDesc, parsedPax, parsedTisch;
+    if (req.body.desc == ""){
+        parsedDesc = " ";
+    }else{
+        parsedDesc = req.body.desc;
+    }
+    if (req.body.pax == ""){
+        parsedPax = "0";
+    }else{
+        parsedPax = String(req.body.pax);
+    }
+    if (req.body.tisch == ""){
+        parsedTisch = "0";
+    }else{
+        parsedTisch = String(req.body.tisch);
+    }
+    if (req.body.date == "" || req.body.time == "" || req.body.name == "") {
+        terror = true;
+
+    } else {
+        var sqlcommand = " INSERT INTO reservierungen Values";
+        var values = '(' + String(req.body.date) + ',' + String(req.body.time) + ',"' + String(req.body.name) + '",' + parsedPax + ',' + parsedTisch + ',' + '"' + parsedDesc + '" )';
+        console.log(sqlcommand, values);
+        db.run(sqlcommand + values);
+    }
     var today = new Date();
     var day = today.getDate();
     var month = today.getMonth();
     var year = today.getFullYear();
 
     var todayRoute = '/check/' + String(day) + '_' + String(month + 1) + '_' + String(year)
-
+    data = { error: terror };
     res.redirect(todayRoute);
 });
 
